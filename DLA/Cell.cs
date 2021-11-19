f6de793dee32855d3b3aaa71fdd5fe1df7e72d05
Kieran200 <ann.funtikova06@mail.ru>
2021-11-19 10:50:25 +0300

    1

---
+++ DLA/Cell.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLA
+{
+    class Cell
+    {
+        public int _x;
+        public int _y;
+        public int[,] _field;
+        public int Type;
+        public Cell (int [,] field, int x, int y)
+        {
+            Type = 1;
+            _field = field;
+            _x = x;
+            _y = y;
+            _field[_x, _y] = Type;
+        }
+        public void Move(int direction, int[,] field, int N, int M)
+        {
+            _field[_x, _y] = 0;
+            switch (direction)
+            {
+                case 0:
+                    {
+                        if (_x + 1 < N && field[_x + 1, _y] == 0)
+                        {
+                            _x = _x + 1;
+                            break;
+                        }
+                        else break;
+                    }
+                case 1:
+                    {
+                        if (_x - 1 > 0  && field[_x - 1, _y] == 0)
+                        {
+                            _x = _x - 1;
+                            break;
+                        }
+                        else break;
+                    }
+                case 2:
+                    {
+                        if (_y - 1 > 0 && field[_x, _y - 1] == 0)
+                        {
+                            _y = _y - 1;
+                            break;
+                        }
+                        else break;
+                    }
+                case 3:
+                    {
+                        if (_y + 1 < M && field[_x, _y + 1] == 0)
+                        {
+                            _y = _y + 1;
+                            break;
+                        }
+                        else break;
+                    }
+            }
+            _field[_x, _y] = Type;
+        }
+
+        public void Neighbor(int [,] field, out int stop, int N, int M)
+        {
+            stop = 0;
+            for (int j = 0; j < 1; j++)
+            {
+                stop = 0;
+                if (field[_x, _y + 1] == 1 && _y + 1 < M)
+                {
+                    stop++;
+                    break;
+                }
+                if (field[_x, _y - 1] == 1 && _y - 1 > 0)
+                {
+                    stop++;
+                    break;
+                }
+                if (field[_x + 1, _y] == 1 && _x + 1 < N)
+                {
+                    stop++;
+                    break;
+                }
+                if (field[_x - 1, _y] == 1 && _x - 1 > 0)
+                {
+                    stop++;
+                    break;
+                }
+                if (field[_x + 1, _y + 1] == 1 && _y + 1 < M && _x + 1 <N)
+                {
+                    stop++;
+                    break;
+                }
+                if (field[_x + 1, _y - 1] == 1 && _y - 1 > 0 && _x + 1 < N)
+                {
+                    stop++;
+                    break;
+                }
+                if (field[_x - 1, _y + 1] == 1 && _y + 1 <M && _x - 1 > 0)
+                {
+                    stop++;
+                    break;
+                }
+                if (field[_x - 1, _y - 1] == 1 && _y - 1 > 0 && _x - 1 >0)
+                {
+                    stop++;
+                    break;
+                }
+            }
+        }
+
+    }
+}
---
+++ DLA/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stop;
+            Random rnd = new Random();
+            Console.WriteLine("Enter the number of lines(odd): ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of columns(odd): ");
+            int M = Convert.ToInt32(Console.ReadLine());
+            int[,] field = new int[N+1, M+1];
+            Random random = new Random();
+
+            field[N / 2, M / 2] = 1;
+
+            while (true)
+            {
+                DrawField();
+            S: Cell cell = new Cell(field, rnd.Next(0, N), rnd.Next(0, M));
+                if (cell._x == N / 2 && cell._y == M / 2)
+                    goto S;
+                while (true)
+                {
+                    cell.Move(rnd.Next(0, 4), field, N, M);
+                    DrawField();
+                    Console.ReadKey();
+                    cell.Neighbor(field, out stop, N, M);
+                    if (stop > 0)
+                    {
+                        break;
+                    }
+                }
+
+                Console.ReadKey();
+            }
+
+
+            void DrawField()        //метод вырисовки поля
+            {
+                Console.Clear();
+                for (int j = 0; j < N; j++)
+                {
+                    for (int i = 0; i < M; i++)
+                    {
+                        if (field[i, j] == 1)
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write(field[i, j]);
+                        Console.ResetColor();
+                    }
+                    Console.WriteLine("");
+                }
+
+            }
+        }
+
+    }
+}
